0986f5a0c3840d817e8810dbd8e59fd64a83f5cc
Carlos Takeshita <ctakeshita@ucsd.edu>
2024-04-20 21:10:33 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Teensy EFI Project
+
+The goal of this project is to implement a fuel injection system based on the Teensy 4.0 microcontroller. The Teensy code will include provisions for metering mass air flow and calculating the appropriate fuel injection load.
+
+## Installation
+
+There will eventually be C++ code in the repository that can be flashed onto Teensy 4.0 through Arduino IDE.
---
+++ test_tooth_detection/test_tooth_detection.ino
@@ -0,0 +1,23 @@
+#define Hall_Switch_Pin 3
+#define Output_LED_Pin 7
+
+void Trigger_Wheel_Tooth_Interrupt() {
+  if (digitalRead(Hall_Switch_Pin) == HIGH) {
+    digitalWrite(Output_LED_Pin, LOW);
+  }
+  else {
+    digitalWrite(Output_LED_Pin, HIGH);
+  }
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(Output_LED_Pin, OUTPUT);
+  pinMode(Hall_Switch_Pin, INPUT);
+  attachInterrupt(digitalPinToInterrupt(Hall_Switch_Pin), Trigger_Wheel_Tooth_Interrupt, CHANGE);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
