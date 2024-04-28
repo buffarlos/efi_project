d48c11afb3de917c0e265e61462809b88a615aed
Carlos Takeshita <ctakeshita@ucsd.edu>
2024-04-27 19:35:11 -0700

    Fix VE table interpolation functionality.
    Made corrections to VE table interpolation code to make it functional.
    Fixed off-by-one errors, lookup table index value errors. Fixed issue
    where interpolation code did not account for the fact tabulated values
    may not start at zero.

--- efi/efi.ino
+++ efi/efi.ino
@@ -15,30 +15,34 @@ byte Old_Tooth_Number = 0; // Tooth number last seen by loop function. If != Too
 // Constants.
 const byte Number_Of_Teeth = 24; // Number of teeth on trigger wheel. INCLUDE MISSING TEETH (e.g. enter 18 for 18-1 trigger wheel, not 17).
 const byte Number_Of_Missing_Teeth = 2; // Number of missing teeth on trigger wheel.
-const byte Number_Of_Actual_Teeth = Number_Of_Teeth - Number_Of_Missing_Teeth;
-const float Degrees_Per_Tooth = 360/Number_Of_Teeth;
+const byte Number_Of_Actual_Teeth = Number_Of_Teeth - Number_Of_Missing_Teeth; // Number of physical teeth on trigger wheel.
+const float Degrees_Per_Tooth = 360/Number_Of_Teeth; // Angle swept out by each trigger wheel tooth, in degrees.
 const float Gap_Detection_Threshold = 1.75; // If new interval is this many times longer than last interval, detect gap.
 const float Injection_Angle = 90; // Crankshaft angle at which injection should occur, in degrees.
 const int VE_Table_Speed_Points = 8; // Number of linearly spaced tabulated crankshaft speed points in VE table.
 const int VE_Table_MAP_Points = 8; // Number of linearly spaced tabulated MAP points in VE table.
 const float VE_Table_Minimum_Speed = 0.003; // Minimum tabulated crankshaft speed in VE table, in degrees per microsecond.
 const float VE_Table_Maximum_Speed = 0.024; // Maximum tabulated crankshaft speed in VE table, in degrees per microsecond.
-const float VE_Table_Speed_Step = (VE_Table_Maximum_Speed - VE_Table_Minimum_Speed)/(VE_Table_Speed_Points - 1);
-const float VE_Table_Minimum_MAP = 0.0; // Minimum tabulated MAP in VE table, in kPa.
-const float VE_Table_Maximum_MAP = 100.0; // Minimum tabulated MAP in VE table, in kPa.
-const float VE_Table_MAP_Step = (VE_Table_Maximum_MAP - VE_Table_Minimum_MAP)/(VE_Table_MAP_Points - 1);
-const float Redline = 0.0216; // Engine speed beyond which fuel will be cut.
+const float VE_Table_Speed_Step = (VE_Table_Maximum_Speed - VE_Table_Minimum_Speed)/(VE_Table_Speed_Points - 1); // Interval between tabulated crankshaft speed values.
+const float VE_Table_Minimum_MAP = 20.0; // Minimum tabulated MAP in VE table, in kPa.
+const float VE_Table_Maximum_MAP = 120.0; // Minimum tabulated MAP in VE table, in kPa.
+const float VE_Table_MAP_Step = (VE_Table_Maximum_MAP - VE_Table_Minimum_MAP)/(VE_Table_MAP_Points - 1); // Interval between tabulated MAP values.
+const float Redline = 0.0216; // Crankshaft speed beyond which fuel will be cut.
 
-// VE Table.
-float VE_Table[VE_Table_Speed_Points][VE_Table_MAP_Points] = {
-  {0.7, 0.7, 0.7, 0.8, 0.8, 0.9, 0.9, 0.8},
-  {0.7, 0.7, 0.7, 0.8, 0.8, 0.9, 0.9, 0.8},
-  {0.7, 0.7, 0.7, 0.8, 0.8, 0.8, 0.8, 0.8},
-  {0.6, 0.7, 0.7, 0.7, 0.8, 0.8, 0.8, 0.8},
-  {0.6, 0.6, 0.6, 0.7, 0.7, 0.8, 0.8, 0.7},
-  {0.5, 0.6, 0.6, 0.7, 0.7, 0.7, 0.7, 0.7},
-  {0.4, 0.4, 0.4, 0.5, 0.5, 0.5, 0.5, 0.4},
-  {0.3, 0.3, 0.3, 0.3, 0.4, 0.4, 0.3, 0.3},
+// VE Tables.
+float VE_Table[VE_Table_MAP_Points][VE_Table_Speed_Points] = {
+  {0.3, 0.3, 0.3, 0.3, 0.4, 0.4, 0.3, 0.3}, // 20.0 kPa
+  {0.4, 0.4, 0.4, 0.5, 0.5, 0.5, 0.5, 0.4}, // 34.3 kPa
+  {0.5, 0.6, 0.6, 0.7, 0.7, 0.7, 0.7, 0.7}, // 48.6 kPa
+  {0.6, 0.6, 0.6, 0.7, 0.7, 0.8, 0.8, 0.7}, // 62.9 kPa
+  {0.6, 0.7, 0.7, 0.7, 0.8, 0.8, 0.8, 0.8}, // 77.1 kPa
+  {0.7, 0.7, 0.7, 0.8, 0.8, 0.8, 0.8, 0.8}, // 91.4 kPa
+  {0.7, 0.8, 0.8, 0.8, 0.8, 0.9, 0.9, 0.8}, // 105.7 kPa
+  {0.7, 0.7, 0.7, 0.8, 0.8, 0.9, 0.9, 0.8}  // 120.0 kPa
+// 500  1000 1500 2000 2500 3000 3500 4000
+};
+float Start_VE_Table[1][VE_Table_MAP_Points] = {
+  {0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9}
 };
 
 // Pin name constants.
@@ -70,22 +74,29 @@ void Trigger_Wheel_Tooth_ISR() {
   Last_Time_Interval = Current_Time_Interval;
 }
 
-unsigned long Injection_Time_Calculation(float Crankshaft_Speed, float MAP) {
+float Injection_Time_Calculation(float Crankshaft_Speed, float MAP) {
   // Takes current crankshaft speed and MAP, and calculates interpolated injection time based on VE table.
-  int Lower_Speed_Index = max(0, min(VE_Table_Speed_Points - 1, (int)(Crankshaft_Speed/VE_Table_Speed_Step)));
-  int Upper_Speed_Index = min(VE_Table_Speed_Points - 1, Lower_Speed_Index - 1);
-  int Lower_MAP_Index = max(0, min(VE_Table_MAP_Points - 1, (int)(MAP/VE_Table_MAP_Step)));
-  int Upper_MAP_Index = min(VE_Table_MAP_Points - 1, Lower_MAP_Index - 1);
-  float Interpolated_VE = interpolation(Crankshaft_Speed, Lower_Speed_Index*VE_Table_Speed_Step, Upper_Speed_Index*VE_Table_Speed_Step,
-    interpolation(MAP, Lower_MAP_Index*VE_Table_MAP_Step, Upper_MAP_Index*VE_Table_MAP_Step,
-      VE_Table[Lower_Speed_Index][Lower_MAP_Index], VE_Table[Lower_Speed_Index][Upper_MAP_Index]),
-    interpolation(MAP, Lower_MAP_Index*VE_Table_MAP_Step, Upper_MAP_Index*VE_Table_MAP_Step,
-      VE_Table[Upper_Speed_Index][Lower_MAP_Index], VE_Table[Upper_Speed_Index][Upper_MAP_Index]));
+  int Lower_MAP_Index = max(0, min(VE_Table_MAP_Points - 1, (int)((MAP - VE_Table_Minimum_MAP)/VE_Table_MAP_Step)));
+  int Upper_MAP_Index = min(VE_Table_MAP_Points - 1, Lower_MAP_Index + 1);
+  float Interpolated_VE;
+  if (Crankshaft_Speed < 0.003) {
+    Interpolated_VE = Interpolation(MAP, (Lower_MAP_Index*VE_Table_MAP_Step + VE_Table_Minimum_MAP), (Upper_MAP_Index*VE_Table_MAP_Step + VE_Table_Minimum_MAP),
+      Start_VE_Table[0][Lower_MAP_Index], Start_VE_Table[0][Upper_MAP_Index]);
+  }
+  else {
+    int Lower_Speed_Index = max(0, min(VE_Table_Speed_Points - 1, (int)((Crankshaft_Speed - VE_Table_Minimum_Speed)/VE_Table_Speed_Step)));
+    int Upper_Speed_Index = min(VE_Table_Speed_Points - 1, Lower_Speed_Index + 1);
+    Interpolated_VE = Interpolation(Crankshaft_Speed, (Lower_Speed_Index*VE_Table_Speed_Step + VE_Table_Minimum_Speed), (Upper_Speed_Index*VE_Table_Speed_Step + VE_Table_Minimum_Speed),
+      Interpolation(MAP, (Lower_MAP_Index*VE_Table_MAP_Step + VE_Table_Minimum_MAP), (Upper_MAP_Index*VE_Table_MAP_Step + VE_Table_Minimum_MAP),
+        VE_Table[Lower_MAP_Index][Lower_Speed_Index], VE_Table[Lower_MAP_Index][Upper_Speed_Index]),
+      Interpolation(MAP, (Lower_MAP_Index*VE_Table_MAP_Step + VE_Table_Minimum_MAP), (Upper_MAP_Index*VE_Table_MAP_Step + VE_Table_Minimum_MAP),
+        VE_Table[Upper_MAP_Index][Lower_Speed_Index], VE_Table[Upper_MAP_Index][Upper_Speed_Index]));
+  }
   // Insert code to translate VE to an injector pulse length.
   return Interpolated_VE;
 }
 
-float interpolation(float x, float xl, float xu, float yl, float yu) {
+float Interpolation(float x, float xl, float xu, float yl, float yu) {
   // Interpolation function for VE table.
   return yl + (yu - yl)*(x - xl)/(xu - xl);
 }
@@ -118,16 +129,20 @@ void loop() {
   }
   if ((Old_Crankshaft_Position < Injection_Angle) && (Crankshaft_Position >= Injection_Angle) && (Crankshaft_Speed < Redline)) {
     // Insert code to translate MAP input value to MAP.
-    Injection_Time = Injection_Time_Calculation(Crankshaft_Speed, MAP);
+    MAP = 95;
+    Injection_Time = 10000*Injection_Time_Calculation(Crankshaft_Speed, MAP);
     digitalWrite(Injector_Signal_Pin, HIGH);
     Injection_Start_Time = micros();
+    Serial.println(Injection_Time);
   }
   if (micros() - Injection_Start_Time >= Injection_Time) {
     digitalWrite(Injector_Signal_Pin, LOW);
   }
   // Diagnostic crankshaft position signal.
+  /*
   if (micros() % 10000 == 0) {
     Serial.println(Crankshaft_Position);
   }
+  */
   Old_Crankshaft_Position = Crankshaft_Position;
 }
